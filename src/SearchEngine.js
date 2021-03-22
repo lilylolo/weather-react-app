b57d17482e2de0c67f8b4dd09339b36ce07b21ad
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-03-22 17:24:32 +0000

    Update

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -10,8 +10,7 @@ export default function SearchEngine() {
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description
+      
     });
   }
 
@@ -39,7 +38,6 @@ export default function SearchEngine() {
         {form}
         <ul>
           <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
           <li>Humidity: {weather.humidity}%</li>
           <li>Wind: {weather.wind}km/h</li>
           <li>
